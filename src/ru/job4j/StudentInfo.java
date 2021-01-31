4eb70b89c7f1770f58926e79534d503cb9e62d7d
Nataliya-nataliya <nataliya.chest@gmail.com>
2021-01-31 17:28:24 +0300

    3.3. Разбор первой программы. [#460427]

--- src/ru/job4j/StudentInfo.java
+++
@@ -1,9 +0,0 @@
-package ru.job4j;
-
-public class StudentInfo {
-    public static void main(String[] args) {
-        System.out.println("Nataliya Razumova");
-        System.out.println("12.02.1992");
-
-    }
-}
