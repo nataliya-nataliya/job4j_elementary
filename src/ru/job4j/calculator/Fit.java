419d409cd5766e12f31672f0da6138c912aa1248
Nataliya-nataliya <nataliya.chest@gmail.com>
2021-02-05 19:43:07 +0300

    6. Идеальный вес [#460407]

--- src/ru/job4j/calculator/Fit.java
+++ src/ru/job4j/calculator/Fit.java
@@ -3,14 +3,12 @@ package ru.job4j.calculator;
 public class Fit {
     public static double manWeight(short height) {
         double rsl;
-        rsl = (height - 100) * 1.15;
-        return rsl;
+        return rsl = (height - 100) * 1.15;
     }
 
     public static double womanWeight(short height) {
         double rsl;
-        rsl = (height - 110) * 1.15;
-        return rsl;
+        return rsl = (height - 110) * 1.15;
     }
 
     public static void main(String[] args) {
