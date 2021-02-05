eba5956e0df944aaf42f38444ad91e171ea4aef6
Nataliya-nataliya <nataliya.chest@gmail.com>
2021-02-05 23:44:44 +0300

    9.1. String [#460417]

--- src/ru/job4j/ru/job4j/condition/Greeting.java
+++ src/ru/job4j/ru/job4j/condition/Greeting.java
@@ -2,7 +2,7 @@ package ru.job4j.ru.job4j.condition;
 
 public class Greeting {
     public static void main(String[] args) {
-        String idea =  "I like Java!";
+        String idea = "I like Java!";
         System.out.println(idea + "But I am a newbie.");
         int year = 2021;
         idea += year;
