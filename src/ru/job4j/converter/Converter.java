4a68120ad077f6d3e2574a5fd724ff9d9f80bc78
Nataliya-nataliya <nataliya.chest@gmail.com>
2021-02-06 16:18:24 +0300

    9.2. Checkstyle [#460423]

--- src/ru/job4j/converter/Converter.java
+++ src/ru/job4j/converter/Converter.java
@@ -10,10 +10,23 @@ public class Converter {
         return value / 60;
     }
 
-    public static void main(String[] args) {
+    /*public static void main(String[] args) {
         int euro = Converter.rubleToEuro(140);
         int dollar = Converter.rubleToDollar(140);
         System.out.println("140 rubles are " + euro + " euro.");
         System.out.println("140 rubles are " + dollar + " dollars.");
+    }*/
+    public static void main(String[] args) {
+        int in = 140;
+        int expected = 2;
+        int out = Converter.rubleToEuro(in);
+        boolean passed = expected == out;
+        System.out.println("140 rubles are 2. Test result : " + passed);
+
+        int in1 = 140;
+        int expected1 = 2;
+        int out1 = Converter.rubleToDollar(in);
+        boolean passed1 = expected1 == out1;
+        System.out.println("140 euro are 2. Test result : " + passed1);
     }
 }
