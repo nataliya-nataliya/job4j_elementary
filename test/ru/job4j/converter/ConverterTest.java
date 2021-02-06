387bd32de05026fc3e6b0392b235f8c35e59476d
Nataliya-nataliya <nataliya.chest@gmail.com>
2021-02-06 17:00:17 +0300

    1. Модульные тесты. [#460440]

--- test/ru/job4j/converter/ConverterTest.java
+++ test/ru/job4j/converter/ConverterTest.java
@@ -3,10 +3,7 @@ package ru.job4j.converter;
 import org.junit.Assert;
 import org.junit.Test;
 
-//import junit.framework.TestCase;
-
-public class ConverterTest  {
-
+public class ConverterTest {
     @Test
     public void whenConvert140RblThen2Euro() {
         int in = 140;
