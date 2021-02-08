a406c1313b3dc72f645b489839c74b4bf9d380a1
Nataliya-nataliya <nataliya.chest@gmail.com>
2021-02-08 23:05:45 +0300

    4.3.4. if c return [#460362]

--- src/ru/job4j/condition/IfElseReturn.java
+++ src/ru/job4j/condition/IfElseReturn.java
@@ -4,7 +4,6 @@ public class IfElseReturn {
     public static boolean eq(int x1, int y1, int x2, int y2) {
         boolean eqX = x1 == x2;
         boolean eqY = y1 == y2;
-
             return eqX && eqY;
     }
 
