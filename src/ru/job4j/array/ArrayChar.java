b516c6f5f5da5e36f3c256ffc49a65016d704400
Nataliya-nataliya <nataliya.chest@gmail.com>
2021-02-18 20:48:02 +0300

    6.4. Слово начинается с ... [#460376]

---
+++ src/ru/job4j/array/ArrayChar.java
@@ -0,0 +1,11 @@
+package ru.job4j.array;
+
+public class ArrayChar {
+    public static boolean startsWith(char[] word, char[] pref) {
+        boolean result = true;
+        for (int i = 0; i < pref.length; i++) {
+            result = (word[i] != pref[i]) ? false : true;
+                  }
+        return result;
+    }
+}
