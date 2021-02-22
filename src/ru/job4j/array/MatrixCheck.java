1e0d0490b4b3768ba844d54956a702dc2ebbcf02
Nataliya-nataliya <nataliya.chest@gmail.com>
2021-02-22 11:24:33 +0300

    6.7.4. Выигрышные комбинации в сокобан [#460378]

--- src/ru/job4j/array/MatrixCheck.java
+++ src/ru/job4j/array/MatrixCheck.java
@@ -31,4 +31,17 @@ public class MatrixCheck {
         }
         return rsl;
     }
+
+    public static boolean isWin(char[][] board) {
+        boolean result = false;
+        for (int i = 0; i < board.length; i++) {
+            if (board[i][i] == 'X') {
+                if ((monoHorizontal(board, i) || monoVertical(board, i)) == true) {
+                    result = true;
+                    break;
+                }
+            }
+        }
+        return result;
+    }
 }
