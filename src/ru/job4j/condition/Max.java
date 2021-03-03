43d3a0ea144ad4ee4e6034680718d74773574986
Nataliya-nataliya <nataliya.chest@gmail.com>
2021-03-03 22:57:10 +0300

    3. Перегрузить метод max для трех чисел [#460566]

--- src/ru/job4j/condition/Max.java
+++ src/ru/job4j/condition/Max.java
@@ -5,6 +5,14 @@ public class Max {
         return left > right ? left : right;
     }
 
+    public static int max(int left, int right, int third) {
+        return max(left, max(right, third));
+    }
+
+    public static int max(int left, int right, int third, int forth) {
+        return max(left, max(right, third, forth));
+    }
+
     public static void main(String[] args) {
     int a = Max.max(5, 1);
         System.out.println(a);
--- test/ru/job4j/condition/MaxTest.java
+++ test/ru/job4j/condition/MaxTest.java
@@ -25,4 +25,34 @@ public class MaxTest {
         int result = Max.max(7, 7);
         assertThat(result, is(7));
     }
+
+    @Test
+    public void whenMax0To3To2Then3() {
+        int result = Max.max(0, 3, 2);
+        assertThat(result, is(3));
+    }
+
+    @Test
+    public void whenMax0To1To7To2Then7() {
+        int result = Max.max(0, 1, 7, 2);
+        assertThat(result, is(7));
+    }
+
+    @Test
+    public void whenMax0To1To2To7Then7() {
+        int result = Max.max(0, 1, 2, 7);
+        assertThat(result, is(7));
+    }
+
+    @Test
+    public void whenMax7To0To1To2Then7() {
+        int result = Max.max(7, 0, 1, 2);
+        assertThat(result, is(7));
+    }
+
+    @Test
+    public void whenMax0To7To1To2Then7() {
+        int result = Max.max(0, 7, 1, 2);
+        assertThat(result, is(7));
+    }
 }
